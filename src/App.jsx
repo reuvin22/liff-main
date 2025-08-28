52cd8cba092753f4953ad50a5a92d0efc6013845
Reuvin <reuvinhernandez22@gmail.com>
2025-08-29 07:58:29 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ function App() {
     <>
       <AdsContext.Provider value={{ isDone, setIsDone, isLoading, setIsLoading, isReady, setIsReady, adsPlaying, setAdsPlaying, isClicked, setIsClicked, countdown, setCountdown, countInterval, setCountInterval, generateIsReady, setGenerateIsReady, compressIsReady, setCompressIsReady }}>
         <Routes>
-            <Route path='/' element={<LoadingScreen />} />
             <Route path="/home" element={<Home />} />
             <Route path='/explanation' element={<Carousel />} />
         </Routes>
