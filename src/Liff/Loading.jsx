8e39d6e65c4b5407cb3060e898ea77bb357a25ca
Reuvin <reuvinhernandez22@gmail.com>
2025-10-11 05:03:52 +0800

    Update

--- src/Liff/Loading.jsx
+++ src/Liff/Loading.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useAdsContext } from "../utils/context";
+import LoadingImage from "../assets/loading.png";
 
 const Loading = ({ generate, prompt }) => {
     const [ads, setAds] = useState(null);
