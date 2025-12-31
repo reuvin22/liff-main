c3276321c0fc135a571d7602e00a076b2e4f2cf3
reuvin22 <reuvinhernandez66@gmail.com>
2025-12-31 16:41:30 +0800

    Update

--- src/Liff/howToUse/Main.jsx
+++ src/Liff/howToUse/Main.jsx
@@ -9,7 +9,7 @@ import Image7 from '../../assets/how-to-use/7.png'
 import Image8 from '../../assets/how-to-use/8.png'
 const cardData = [
   {
-    header: "1-1. ガクチカ",
+    header: "1-1 ガクチカ",
     instruction: `志望動機/自己PR生成`,
     image: Image1,
   },
@@ -19,41 +19,18 @@ const cardData = [
     image: Image2,
   },
   {
-    header: `2-1. 志望企業`,
-    instruction: "質問1、2では自分に当てはまると思うものを選択してください。",
+    header: `1-3. ガクチカ`,
+    instruction: "志望動機/自己PR生成",
     image: Image3,
   },
   {
-    header: `2-2. 志望企業`,
-    instruction: "質問3からは自由記述形式です。書き方の助言も参考に。",
+    header: `2-1. 志望企業`,
     image: Image4,
   },
     {
-    header: `2-3. ガクチカ/志望動機/
-自己PR生成`,
-    instruction: `書き方の助言を参考にしながら
-入力することも可能です。`,
+    header: `2-2. 志望企業`,
     image: Image5,
-  },
-    {
-    header: `2-4. ガクチカ/志望動機/
-自己PR生成`,
-    instruction: `終了したらAIの応答を少し待ちましょう。`,
-    image: Image6,
-  },
-      {
-    header: `2-5. ガクチカ/志望動機/
-自己PR生成`,
-    instruction: `終了したらお好みの方法でAIの
-生成文章を保存をしましょう。`,
-    image: Image7,
-  },
-   {
-    header: `3-1. 友達に紹介`,
-    instruction: `回答お疲れ様でした。就活仲間
-にも共有してみてください。`,
-    image: Image8,
-  },
+  }
 ];
 
 function Main() {
